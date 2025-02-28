79458db579778f1992185e23ada131d762100272
Elisa <emscouto89@gmail.com>
2025-02-28 17:01:35 +0000

    Update membro.py

--- classes/membro.py
+++ classes/membro.py
@@ -3,3 +3,12 @@ class Membro:
         self.nome = nome
         self.email = email
         self.funcao = funcao
+
+    def alterar_nome(self, novo_nome):
+        self.nome = novo_nome
+
+    def alterar_email(self, novo_email):
+        self.email = novo_email
+    
+    def alterar_funcao(self, nova_funcao):
+        self.funcao = nova_funcao
