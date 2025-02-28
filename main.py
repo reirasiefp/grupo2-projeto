f5ca7903994abe6422d29b9a6afe54f6a619f764
rafaeleiras <7146133@formacao.iefp.pt>
2025-02-28 15:47:31 +0000

    menu inicial

--- main.py
+++ main.py
@@ -10,3 +10,17 @@ print(tarefa)
 membro = Membro("nome", "email", "funcao")
 
 projeto = Projeto("nome")
+
+
+print("menu inicial:")
+while True:
+    print("1. Continuar:")
+    print("2. Sair:")
+    escolha = int(input("Escolha:"))
+
+    if escolha == 1:
+        continue
+    elif escolha == 2:
+        break
+    else:
+        print("Numero errado.")
