3e552aa69eabf2e2d42062d538f111026e02be92
rafaeleiras <7146133@formacao.iefp.pt>
2025-02-28 17:13:31 +0000

    Lógica relações de tabelas, Tabela tarefa update

---
+++ classes/tarefa_membro.py
@@ -0,0 +1,4 @@
+class Tarefa_Membro:
+    def __init__(self, id_tarefa, email_membro):
+        self.id_tarefa = id_tarefa
+        self.email_membro = email_membro
--- main.py
+++ main.py
@@ -4,9 +4,19 @@ from classes.tarefa import Tarefa
 
 #menus
 
-tarefa = Tarefa("nome_da tarefa", 1, "tipo", "2025-02-28")
+tarefa = Tarefa("nome da tarefa", 1, "tipo", "2025-05-01")
 print(tarefa)
+print("#"*30)
 
-membro = Membro("nome", "email", "funcao")
+
+membro = Membro("nome1", "email1", "funcao1")
+tarefa.adicionar_membro(membro)
+membro = Membro("nome1", "email1", "funcao1")
+tarefa.adicionar_membro(membro)
+
+membro2 = Membro("nome2", "email2", "funcao2")
+tarefa.adicionar_membro(membro2)
+
+tarefa.verListaMembros()
 
 projeto = Projeto("nome")
